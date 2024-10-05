626631c252606b039228feffb94b6e7160d0c31e
Bubsyyy <lubomirlubomirovkn123@gmail.com>
2024-10-06 00:15:54 +0300

    Initial commit

---
+++ Java Advanced/_03_Sets_And_Maps_Advanced/PeriodicTable.java
@@ -0,0 +1,17 @@
+package _03_Sets_And_Maps_Advanced;
+
+import java.util.*;
+
+public class PeriodicTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<String> uniqueElements = new TreeSet<>();
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int row = 1; row <= n; row++) {
+            String []chemicalElements = scanner.nextLine().split("\\s+");
+            uniqueElements.addAll(Arrays.asList(chemicalElements));
+        }
+        uniqueElements.forEach(el -> System.out.print(el + " "));
+    }
+}
