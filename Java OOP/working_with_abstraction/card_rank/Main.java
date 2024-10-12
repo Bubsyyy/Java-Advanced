76d0b0de202df6134d11bfe5aa2af0cd1f7cb79e
Bubsyyy <lubomirlubomirovkn123@gmail.com>
2024-10-12 15:52:42 +0300

    Initial commit

---
+++ Java OOP/working_with_abstraction/card_rank/Main.java
@@ -0,0 +1,10 @@
+package working_with_abstraction.card_rank;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CardRank.printCardRank();
+
+
+    }
+}
---
+++ Java OOP/working_with_abstraction/card_suit/Main.java
@@ -0,0 +1,10 @@
+package working_with_abstraction.card_suit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       CardSuit.printCardSuits();
+
+    }
+}
