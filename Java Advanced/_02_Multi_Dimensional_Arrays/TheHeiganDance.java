5f7adbbf98f745f5231ac813863e024dbb55b67d
Bubsyyy <lubomirlubomirovkn123@gmail.com>
2024-12-13 18:50:33 +0200

    The Heigan dance

--- Java Advanced/_02_Multi_Dimensional_Arrays/TheHeiganDance.java
+++ Java Advanced/_02_Multi_Dimensional_Arrays/TheHeiganDance.java
@@ -4,70 +4,101 @@ import java.util.Scanner;
 
 public class TheHeiganDance {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        int playerRow = 7;
-        int playerCol = 7;
-
-        int playerHP = 18500;
-        double heiganHP = 3000000;
-        double playerDamage = Double.parseDouble(scan.nextLine());
+        double hsPoints = 3000000.0;
+        int playerPoints = 18500;
+        int startPlRow = 7;
+        int startPlCol = 7;
         String lastSpell = "";
-        while (true) {
-            if (playerHP >= 0) {
-                heiganHP -= playerDamage;
+        boolean activeCloud = false;
+
+        double damage = Double.parseDouble(scanner.nextLine());
+
+        while (playerPoints > 0 && hsPoints > 0) {
+            hsPoints -= damage;
+            if (activeCloud) {
+                playerPoints -= 3500;
+                activeCloud = false;
+
+                if (playerPoints < 0) {
+                    break;
+                }
             }
-            if (lastSpell.equals("Cloud")) {
-                playerHP -= 3500;
-            }
-            if (heiganHP <= 0 || playerHP <= 0) {
+
+            if (hsPoints < 0) {
                 break;
             }
-            String[] input = scan.nextLine().split(" ");
-            String currentSpell = input[0];
-            int targetRow = Integer.parseInt(input[1]);
-            int targetCol = Integer.parseInt(input[2]);
-            if (isInDamageArea(targetRow, targetCol, playerRow, playerCol)) {
-                if (!isInDamageArea(targetRow, targetCol, playerRow - 1, playerCol) && !isWall(playerRow - 1)) {
-                    playerRow--;
-                    lastSpell = "";
-                } else if (!isInDamageArea(targetRow, targetCol, playerRow, playerCol + 1) && !isWall(playerCol + 1)) {
-                    playerCol++;
-                    lastSpell = "";
-                } else if (!isInDamageArea(targetRow, targetCol, playerRow + 1, playerCol) && !isWall(playerRow + 1)) {
-                    playerRow++;
-                    lastSpell = "";
-                } else if (!isInDamageArea(targetRow, targetCol, playerRow, playerCol - 1) && !isWall(playerCol - 1)) {
-                    playerCol--;
-                    lastSpell = "";
-                } else {
-                    if (currentSpell.equals("Cloud")) {
-                        playerHP -= 3500;
-                        lastSpell = "Cloud";
-                    } else if (currentSpell.equals("Eruption")) {
-                        playerHP -= 6000;
-                        lastSpell = "Eruption";
+
+            String[] tokens = scanner.nextLine().split("\\s+");
+            String spell = tokens[0];
+            int row = Integer.parseInt(tokens[1]);
+            int col = Integer.parseInt(tokens[2]);
+
+            boolean[][] hsChamber = new boolean[15][15];
+            for (int i = row - 1; i <= row + 1; i++) {
+                if (i >= 0 && i < hsChamber.length) {
+
+                    for (int j = col - 1; j <= col + 1; j++) {
+                        if (j >= 0 && j < hsChamber[row].length) {
+
+                            hsChamber[i][j] = true;
+                        }
+                    }
+                }
+            }
+
+            if (hsChamber[startPlRow][startPlCol]) {
+                if (isRowValid(hsChamber, startPlRow - 1) && !hsChamber[startPlRow - 1][startPlCol]) {
+                    startPlRow--; // move up
+                } else if (isColValid(hsChamber, startPlCol + 1) && !hsChamber[startPlRow][startPlCol + 1]) {
+                    startPlCol++; // move right
+                } else if (isRowValid(hsChamber, startPlRow + 1) && !hsChamber[startPlRow + 1][startPlCol]) {
+                    startPlRow++; // move down
+                } else if (isColValid(hsChamber, startPlCol - 1) && !hsChamber[startPlRow][startPlCol - 1]) {
+                    startPlCol--; // move left
+                }
+
+                if (hsChamber[startPlRow][startPlCol]) {
+                    switch (spell) {
+                        case "Cloud":
+                            playerPoints -= 3500;
+                            activeCloud = true;
+                            lastSpell = "Plague Cloud";
+                            break;
+                        case "Eruption":
+                            playerPoints -= 6000;
+                            lastSpell = spell;
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Invalid spell: " + spell);
                     }
                 }
             }
         }
-        lastSpell = lastSpell.equals("Cloud") ? "Plague Cloud" : "Eruption";
-        String heiganState = heiganHP <= 0 ? "Heigan: Defeated!" : String.format("Heigan: %.2f", heiganHP);
-        String playerState = playerHP <= 0 ? String.format("Player: Killed by %s", lastSpell) :
-                String.format("Player: %d", playerHP);
-        String playerEndCoordinates = String.format("Final position: %d, %d", playerRow, playerCol);
 
-        System.out.println(heiganState);
-        System.out.println(playerState);
-        System.out.println(playerEndCoordinates);
+        if (hsPoints > 0) {
+            System.out.printf("Heigan: %.2f%n", hsPoints);
+        } else {
+            System.out.println("Heigan: Defeated!");
+        }
+
+        if (playerPoints > 0) {
+            System.out.printf("Player: %d%n", playerPoints);
+        } else {
+            System.out.println("Player: Killed by " + lastSpell);
+        }
+
+        System.out.println("Final position: " + startPlRow + ", " + startPlCol);
     }
 
-    private static boolean isWall(int moveCell) {
-        return moveCell < 0 || moveCell >= 15;
+    private static boolean isRowValid(boolean[][] hsChamber, int startPlRow) {
+        return startPlRow >= 0 && startPlRow < hsChamber[startPlRow].length;
     }
 
-    private static boolean isInDamageArea(int targetRow, int targetCol, int moveRow, int moveCol) {
-        return ((targetRow - 1 <= moveRow && moveRow <= targetRow + 1)
-                && (targetCol - 1 <= moveCol && moveCol <= targetCol + 1));
+    private static boolean isColValid(boolean[][] hsChamber, int startPlCol) {
+        return startPlCol >= 0 && startPlCol < hsChamber.length;
     }
+
+
 }
