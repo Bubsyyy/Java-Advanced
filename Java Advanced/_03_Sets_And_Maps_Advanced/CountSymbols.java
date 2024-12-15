3273802deee4eb1ce4fdc5e4c147fe1572cc129f
Bubsyyy <lubomirlubomirovkn123@gmail.com>
2024-12-15 23:17:27 +0200

    Symbols and phonebook

--- Java Advanced/_03_Sets_And_Maps_Advanced/CountSymbols.java
+++ Java Advanced/_03_Sets_And_Maps_Advanced/CountSymbols.java
@@ -7,17 +7,23 @@ import java.util.TreeMap;
 public class CountSymbols {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<Character, Integer> symbolOccurrenceMap = new TreeMap<>();
-        char[] symbols = scanner.nextLine().toCharArray();
-        for (char symbol : symbols) {
-            if (!symbolOccurrenceMap.containsKey(symbol)){
-                symbolOccurrenceMap.put(symbol, 1);
-            }else {
-                symbolOccurrenceMap.put(symbol, symbolOccurrenceMap.get(symbol) + 1);
+
+        String text = scanner.nextLine();
+        Map<Character, Integer> symbols = new TreeMap<>();
+
+        for (int index = 0; index <= text.length() - 1; index++) {
+            char currentSymbol  = text.charAt(index);
+            if (!symbols.containsKey(currentSymbol)) {
+                symbols.put(currentSymbol, 1);
+            } else {
+                int currentCount = symbols.get(currentSymbol);
+                symbols.put(currentSymbol, currentCount + 1);
             }
         }
-        for (Map.Entry<Character, Integer> entry : symbolOccurrenceMap.entrySet()) {
-            System.out.printf("%c: %d time/s%n", entry.getKey(), entry.getValue());
+
+
+        for (Map.Entry<Character, Integer> entry : symbols.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue() + " time/s");
         }
 
     }
--- Java Advanced/_03_Sets_And_Maps_Advanced/Phonebook.java
+++ Java Advanced/_03_Sets_And_Maps_Advanced/Phonebook.java
@@ -1,5 +1,6 @@
 package _03_Sets_And_Maps_Advanced;
 
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -7,20 +8,28 @@ import java.util.Scanner;
 public class Phonebook {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
         Map<String, String> phonebook = new HashMap<>();
-        String[] input = scanner.nextLine().split("-");
-        while (!input[0].equals("search")) {
-            phonebook.put(input[0], input[1]);
-            input = scanner.nextLine().split("-");
+
+        while (!input.equals("search")) {
+            String name = input.split("-")[0];
+            String phoneNumber = input.split("-")[1];
+            phonebook.put(name, phoneNumber);
+
+            input = scanner.nextLine();
         }
-        input = scanner.nextLine().split("-");
-        while (!input[0].equals("stop")) {
-            if (phonebook.containsKey(input[0])){
-                System.out.println(input[0] + " -> " + phonebook.get(input[0]));
-            }else {
-                System.out.printf("Contact %s does not exist.%n", input[0]);
+
+        String name = scanner.nextLine();
+        while (!name.equals("stop")) {
+            if (phonebook.containsKey(name)) {
+                System.out.printf("%s -> %s%n", name, phonebook.get(name));
+            } else {
+                System.out.printf("Contact %s does not exist.%n", name);
             }
-            input = scanner.nextLine().split("-");
+
+            name = scanner.nextLine();
         }
+
     }
 }
