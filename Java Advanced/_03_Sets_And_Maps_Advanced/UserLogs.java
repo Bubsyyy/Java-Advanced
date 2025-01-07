04b99da60be689d3f443f2450744aa6ec8519004
Bubsyyy <lubomirlubomirovkn123@gmail.com>
2025-01-07 15:08:40 +0200

    User logs

--- Java Advanced/_03_Sets_And_Maps_Advanced/UserLogs.java
+++ Java Advanced/_03_Sets_And_Maps_Advanced/UserLogs.java
@@ -6,32 +6,46 @@ import java.util.Scanner;
 import java.util.TreeMap;
 
 public class UserLogs {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Map<String, Map<String, Integer>> userLogs = new TreeMap<>();
-        String[] input = scanner.nextLine().split(" ");
-        while (!input[0].equals("end")){
-            String user = input[2].substring(5);
-            String IP = input[0].substring(3);
-            if (!userLogs.containsKey(user)){
-                userLogs.put(user, new LinkedHashMap<>());
-            }
-            if (!userLogs.get(user).containsKey(IP)){
-                userLogs.get(user).put(IP, 1);
-            }else {
-                userLogs.get(user).put(IP, userLogs.get(user).get(IP) + 1);
-            }
-            input = scanner.nextLine().split(" ");
-        }
-        for (Map.Entry<String, Map<String, Integer>> user : userLogs.entrySet()) {
-            System.out.println(user.getKey() + ":");
-            String currentUserAddressOutput = "";
-            for (Map.Entry<String, Integer> address : user.getValue().entrySet()) {
-                currentUserAddressOutput = currentUserAddressOutput.concat(address.getKey()).concat(" => ").concat(String.valueOf(address.getValue())).concat(", ");
-            }
-            currentUserAddressOutput = currentUserAddressOutput.substring(0, currentUserAddressOutput.length() - 2);
-            System.out.println(currentUserAddressOutput + ".");
-        }
 
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            String input = scanner.nextLine();
+            Map<String, LinkedHashMap<String, Integer>> messagesInfo = new TreeMap<>();
+
+            while (!input.equals("end")) {
+                String ip = input.split("\\s+")[0].split("\\=")[1];
+                String message = input.split("\\s+")[1].split("\\=")[1];
+                String user = input.split("\\s+")[2].split("\\=")[1];
+
+                messagesInfo.putIfAbsent(user, new LinkedHashMap<>());
+                LinkedHashMap<String, Integer> ipInfo = messagesInfo.get(user);
+
+                if (!messagesInfo.containsKey(user)) {
+                    ipInfo.put(ip, 1);
+                } else {
+                    if (!ipInfo.containsKey(ip)) {
+                        ipInfo.put(ip, 1);
+                    } else {
+                        ipInfo.put(ip, ipInfo.get(ip) + 1);
+                    }
+                }
+
+                input = scanner.nextLine();
+            }
+
+            for (var entry : messagesInfo.entrySet()) {
+                System.out.println(entry.getKey() + ": ");
+                LinkedHashMap<String, Integer> messageIP = entry.getValue();
+
+
+                int count = 1;
+                for (var innerMap : messageIP.entrySet()) {
+                    String output = String.format(count < messageIP.entrySet().size() ? "%s => %d, " : "%s => %d.%n", innerMap.getKey(), innerMap.getValue());
+                    System.out.printf("%s", output);
+                    count++;
+                }
+            }
+
+        }
     }
-}
